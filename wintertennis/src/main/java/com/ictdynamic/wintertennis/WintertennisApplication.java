aee28f7845caa2272d8dd6f470e8ec1222ed96f7
wvdbrand <WVandenBrande@isabelgroup.eu>
2024-05-28 17:06:31 +0200

    first commit

---
+++ wintertennis/src/main/java/com/ictdynamic/wintertennis/WintertennisApplication.java
@@ -0,0 +1,13 @@
+package com.ictdynamic.wintertennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WintertennisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WintertennisApplication.class, args);
+	}
+
+}
---
+++ wintertennis/src/test/java/com/ictdynamic/wintertennis/WintertennisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ictdynamic.wintertennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WintertennisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
